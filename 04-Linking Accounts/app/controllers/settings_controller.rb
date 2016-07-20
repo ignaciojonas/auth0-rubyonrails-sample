5574feb1cf728b927b3ab252a52a560c758abe9d
Ignacio Jonas <ignaciojonas@gmail.com>
2016-07-20 12:02:29 -0300

    WIP - Refactor - Providers are not working.

--- 04-Linking Accounts/app/controllers/settings_controller.rb
+++ 04-Linking Accounts/app/controllers/settings_controller.rb
@@ -1,35 +1,22 @@
-# frozen_string_literal: true
 class SettingsController < SecuredController
+attr_accessor :user, :client
   def show
     @user = session[:userinfo]
     @providers = user_providers.keys.collect { |x| [x, x] }
-  end
-
-  def link_provider
-    user = session[:userinfo]
-    link_user = session[:linkuserinfo]
-
-    v2_creds = { client_id: ENV['AUTH0_CLIENT_ID'],
-                 token: user[:credentials][:id_token],
-                 api_version: 2,
-                 domain: ENV['AUTH0_DOMAIN'] }
-
-    client = Auth0Client.new(v2_creds)
-
-    client.link_user_account(user['uid'], link_with: link_user[:credentials][:id_token])
-
-    redirect_to '/settings', notice: 'Provider succesfully linked.'
-  end
-
-  def unlink_provider
-    user = session[:userinfo]
-
     creds = { client_id: ENV['AUTH0_CLIENT_ID'],
                  token: user[:credentials][:id_token],
                  api_version: 2,
                  domain: ENV['AUTH0_DOMAIN'] }
 
-    client = Auth0Client.new(creds)
+    @client = Auth0Client.new(creds)
+  end
+
+  def link_provider
+    client.link_user_account(user['uid'], link_with: link_user[:credentials][:id_token])
+    redirect_to '/settings', notice: 'Provider succesfully linked.'
+  end
+
+  def unlink_provider
     result = client.unlink_users_account(user['uid'], params['unlink_provider'], user_providers[params['unlink_provider']])
     redirect_to '/settings', notice: 'Provider succesfully unlinked.'
   end
@@ -37,8 +24,6 @@ class SettingsController < SecuredController
   private
 
   def user_providers
-    # TODO: why is it not working with @user?
-    user = session[:userinfo]
     Hash[user['extra']['raw_info']['identities'].collect { |identity| [identity['provider'], identity['user_id']] }]
   end
 end
