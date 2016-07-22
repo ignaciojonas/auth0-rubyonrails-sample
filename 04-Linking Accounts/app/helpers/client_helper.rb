b68bc75a6bdaf7832f8465afec7ad008e3f3c23d
Ignacio Jonas <ignaciojonas@gmail.com>
2016-07-22 09:43:42 -0300

    Add Master JWT to App Secrets

--- 04-Linking Accounts/app/helpers/client_helper.rb
+++ 04-Linking Accounts/app/helpers/client_helper.rb
@@ -1,29 +1,28 @@
 # frozen_string_literal: true
 module ClientHelper
   def self.client_user(user)
-    creds = { client_id: ENV['AUTH0_CLIENT_ID'],
+    creds = { client_id: Rails.application.secrets.auth0_client_id,
               token: user[:credentials][:token],
               api_version: 2,
-              domain: ENV['AUTH0_DOMAIN'] }
+              domain: Rails.application.secrets.auth0_domain }
 
     Auth0Client.new(creds)
   end
 
   def self.client(user)
-    creds = { client_id: ENV['AUTH0_CLIENT_ID'],
+    creds = { client_id: Rails.application.secrets.auth0_client_id,
               token: user[:credentials][:id_token],
               api_version: 2,
-              domain: ENV['AUTH0_DOMAIN'] }
+              domain: Rails.application.secrets.auth0_domain }
 
     Auth0Client.new(creds)
   end
 
   def self.client_admin
-    creds = { client_id: ENV['AUTH0_CLIENT_ID'],
-              token: ENV['AUTH0_MASTER_JWT'],
+    creds = { client_id: Rails.application.secrets.auth0_client_id,
+              token: Rails.application.secrets.auth0_master_jwt,
               api_version: 2,
-              domain: ENV['AUTH0_DOMAIN'] }
-
+              domain: Rails.application.secrets.auth0_domain }
     Auth0Client.new(creds)
   end
 end
