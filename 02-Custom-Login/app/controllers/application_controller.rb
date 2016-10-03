2600ae96f1e46113767073f42ee8e8474f6bc6c1
Ignacio Jonas <ignaciojonas@gmail.com>
2016-10-03 17:02:13 +0200

    Change client to auth0 client.

--- 02-Custom-Login/app/controllers/application_controller.rb
+++ 02-Custom-Login/app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  attr_reader :client
+  attr_reader :auth0_client
 
-  def client
+  def auth0_client
     creds = { client_id: Rails.application.secrets.auth0_client_id,
               client_secret: Rails.application.secrets.auth0_client_secret,
               api_version: 1,
               domain: Rails.application.secrets.auth0_domain }
-    @client ||= Auth0Client.new(creds)
+    @auth0_client ||= Auth0Client.new(creds)
   end
 end
